0b50570b5497f13735419472ea5eecdb94819004
Glitch (glitch-blank-react) <none>
2021-11-12 15:46:49 +0000

    👱🍛 Checkpoint ./svelte.config.js:15176242/3962 ./package.json:15176242/1678 ./watch.json:15176242/231

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,19 @@
-import adapter as static from '@sveltejs/adapter-static';
-
-var adapterConfig = {};
-
-if (process.env.STATIC) {
-  
-} else {
-  
-}
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({ fallback: 'index.html' })
+	},
+	vite: {
+		server: {
+			strictPort: true,
+			hmr: {
+				port: 443 // Run the websocket server on the SSL port
+			}
+		}
 	}
 };
 
