3d8d6768e5a8db93fdc3ae2e15af198a6e4833fc
Glitch (deserted-erratic-viscountess) <none>
2022-04-15 10:16:59 +0000

    🚙🐈 Checkpoint ./src/routes/index.svelte:43080505/174

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,5 @@
-<script>
-  let message = "Hello World!";
-</script>
-
 <section class="p-4">
   <h1>Welcome to SvelteKit</h1>
   <p>This is your homepage.</p>
-
-  {#if message}
-  <p id="message">{message}</p>
-  {/if}
 </section>
 
-<style>
-  #message {
-    font-weight: bold;
-  }
-</style>
