baf231a48539449c8725803d7fc55b963ce3a8e1
Glitch (deserted-erratic-viscountess) <none>
2022-05-07 03:07:11 +0000

    🏍⛲️ Checkpoint ./src/routes/index.svelte:43080505/455 ./src/routes/login.svelte:43080505/1385

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,16 @@
-<section class="p-4">
-  <h1>Welcome to SvelteKit</h1>
-  <p>This is your homepage.</p>
-</section>
+<script>
+    import supabase from '$lib/db';
 
+    async function logout() {
+   	 const { error } = await supabase.auth.signOut();
+
+   	 if (error) alert(error.message); // alert if error
+    }
+
+</script>
+
+
+<!-- Sign Out -->
+<section class="container px-4 py-3 text-center">
+    <button class="btn btn-secondary" on:click={logout}>Logout</button>
+</section>	
