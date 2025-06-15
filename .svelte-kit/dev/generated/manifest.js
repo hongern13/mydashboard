d7e2745ee403b3d7a4a3a3b4da12b034eef58fee
Glitch (deserted-erratic-viscountess) <none>
2025-06-15 10:47:04 +0000

    🍍😼 Checkpoint ./src/firebase.js:63816714/352

--- .svelte-kit/dev/generated/manifest.js
+++ .svelte-kit/dev/generated/manifest.js
@@ -1,14 +1,18 @@
 const c = [
 	() => import("../components/layout.svelte"),
 	() => import("../components/error.svelte"),
-	() => import("../../../src/routes/index.svelte")
+	() => import("../../../src/routes/index.svelte"),
+	() => import("../../../src/routes/login.svelte")
 ];
 
 const d = decodeURIComponent;
 
 export const routes = [
 	// src/routes/index.svelte
-	[/^\/$/, [c[0], c[2]], [c[1]]]
+	[/^\/$/, [c[0], c[2]], [c[1]]],
+
+	// src/routes/login.svelte
+	[/^\/login\/?$/, [c[0], c[3]], [c[1]]]
 ];
 
 // we import the root layout/error components eagerly, so that
--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,12 @@ import { getAuth } from "firebase/auth";
 
 //TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-   //...
+  apiKey: "AIzaSyDmimxnkllR13JmP81XvbXq3a2yOHkrKOQ",
+  authDomain: "my-dashboard-913be.firebaseapp.com",
+  projectId: "my-dashboard-913be",
+  storageBucket: "my-dashboard-913be.firebasestorage.app",
+  messagingSenderId: "996416230173",
+  appId: "1:996416230173:web:a402a57a90f6e13f7c47f4"
 };
 
 export const firebase = initializeApp(firebaseConfig);
